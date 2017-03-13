b136c32dd79c94295ac098a90c7d43370f2fb622
Dominykas Gudonis <donprayer@gmail.com>
2017-03-13 16:58:15 +0200

    Added Input class to read user input

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Input {
+	private Scanner reader = new Scanner(System.in);
+	
+	public void Input() {
+	System.out.println("Enter something: ");
+	String text = reader.nextLine();
+}
+}

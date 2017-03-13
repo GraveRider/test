4608875f75a944145b59eb1be79119178ed25806
Dominykas Gudonis <donprayer@gmail.com>
2017-03-13 17:20:43 +0200

    Added local date

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,13 @@ package firstlesson;
  * @author zwaclaw
  *
  */
+import java.time.localDate
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate localDate = LocalDate.now();
+		System.out.println(dtf.format(localDate));
 		System.out.println("HELLO UGLY WORLD!");
 		System.out.println("The World is not ugly!");
 	}

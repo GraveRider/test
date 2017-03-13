8e04933e85f12efa25e4d440d14774250baf67df
Dominykas Gudonis <donprayer@gmail.com>
2017-03-13 17:33:06 +0200

    Resolved file conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,10 +8,16 @@ import java.time.localDate
 
 public class HelloWorld {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 		LocalDate localDate = LocalDate.now();
 		System.out.println(dtf.format(localDate));
+=======
+		String name = Input.input();
+
+>>>>>>> testing
 		System.out.println("HELLO UGLY WORLD!");
 		System.out.println("The World is not ugly!");
+		System.out.println("Hello " + name + "!");
 	}
 }
--- Input.java
+++ Input.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 class Input {
 	private Scanner reader = new Scanner(System.in);
 	
-	public void Input() {
+	public static String Input() {
 	System.out.println("Enter something: ");
 	String text = reader.nextLine();
+	return text;
 }
 }
